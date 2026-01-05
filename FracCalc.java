ead50517aa9074daeeaba2d7cfc46e88f5f4a746
Aarush-Sengupta11 <2002674@apps.nsd.org>
2026-01-05 21:39:36 +0000

    done

--- FracCalc.java
+++ FracCalc.java
@@ -82,60 +82,241 @@ public class FracCalc {
    //        2 1/8
    //        2 1/4
    public static String processExpression(String input) {
-      // TODO: implement this method!
+      //Uses a scanner to parse the input into two terms and the operator
       Scanner parser = new Scanner(input);
-      parser.next();
+      String term1 = parser.next();
       String operator = parser.next();
-      String x = parser.next();
-      String num = "0";
-      String den = "1";
-      String whole = "0";
+      String term2 = parser.next();
+      parser.close();
+      //Finds and declares each part of the expression as an int (numerators, denominators, whole numbers)
+      int num1 = findNum(term1);
+      int num2 = findNum(term2);
+      int den1 = findDen(term1);
+      int den2 = findDen(term2);
+      int whole1 = findWhole(term1);
+      int whole2 = findWhole(term2);
+      
 
-     /*   if (x.length() == 3) {
-         num = Character.toString(x.charAt(0));
-         den = Character.toString(x.charAt(2));
+      //Change any mixed numbers to improper fractions
+      if (whole1 != 0) {
+         if (whole1 < 0) {
+            num1 = (den1 * whole1) - num1;
+         } else {
+            num1 = (den1 * whole1) + num1;
+         }
+         
+         whole1 = 0;       
       }
-      else if (x.length() == 5) {
-         whole = Character.toString(x.charAt(0));
-         num = Character.toString(x.charAt(2));
-         den = Character.toString(x.charAt(4));
+      if (whole2 != 0) {
+         if (whole2 < 0) {
+            num2 = (den2 * whole2) - num2;
+         } else {
+            num2 = (den2 * whole2) + num2;
+         }
+         
+         whole2 = 0;
+         
       }
-         */
-      if (x.indexOf('/') == -1) {
-         whole = Character.toString(x.charAt(0));
+
+      //If the denominator is less than zero then multiply both numerator and denominator by -1 (also gets rid of negatives if negatives are present on both the numerator and the denominator)
+      if (den1 < 0) {
+         num1 *= -1;
+         den1 *= -1;
       }
+      if (den2 < 0) {
+         num2 *= -1;
+         den2 *= -1;
+      }
+      
+     //Find numResult and den result. Set wholeResult to 0 for now
+      String result =  "";
+      int numResult = findNumResult(num1, den1, num2, den2, operator);
+      int denResult = findDenResult(num1, den1, num2, den2, operator);
+      int wholeResult = 0;
+
+      //if any denominator is zero or den result is zero then result should be "Can't divide by zero!"
+      if (den1 == 0 || den2 == 0 || operator.equals("/") && denResult == 0) {
+         result = "Can't divide by zero!";
+      }
+      //else simplify and format the results
       else {
-         //num = Character.toString(x.charAt(x.indexOf('/')-1));
+         result = Simplify(numResult, denResult, wholeResult, result);
+      }
+     
+      
+
+      
+      //return the result of the expression
+      return result;
+   }
+
+   //Finds the whole number in the expression
+   public static int findWhole(String x) {
+     
+     String whole = "0";
+     //If the expression doesn't include a fraction set the second term to the entire thing
+     if (x.indexOf('/') == -1) {
+         whole = x.substring(0, x.length());
+      }
+      //If the expression does includes a fraction and has an underscore set the whole number to the text before the '_'
+      else {
+         if (x.indexOf('_') != -1) {
+            whole = x.substring(0, (x.indexOf('_')));
+         }
+      }
+      return Integer.parseInt(whole);
+
+   }
+
+   //Finds the numerator in the expression
+   public static int findNum(String x) {
+      String num = "0";
+      //If the expression does include a fraction and has an underscore set the numerator to the text between the '_' and the '/'
+      if (x.indexOf('/') != -1) {
          
          if (x.indexOf('_') != -1) {
-            //whole = Character.toString(x.charAt(x.indexOf('_')-1));
-            whole = x.substring(0, (x.indexOf('_')));
             num = x.substring((x.indexOf('_')+1), (x.indexOf('/')));
          }
+         //If the expression doesn't have an underscore set the numerator to all text before the '/' sign
          else {
             num = x.substring(0, x.indexOf('/'));
          }
-         
-         //den = Character.toString(x.charAt(x.indexOf('/')+1));
-         den = x.substring((x.indexOf('/')+1), x.length());
-
-         if (num.charAt(0) == '-' && den.charAt(0) == '-') {
-            num = num.substring(1, num.length());
-            den = den.substring(1, den.length());
-         }
+              
       }
-      
-      return "Op:" + operator + " Whole:" + whole + " Num:" + num + " Den:" + den;
+      return Integer.parseInt(num);
+   }
+
+   //Finds the denominator in the expression
+   public static int findDen(String x) {
+      String den = "1";
+      //If the expression does include a fraction set the denominator to all text after the '/' sign
+      if (x.indexOf('/') != -1) {
+         den = x.substring((x.indexOf('/')+1), x.length());
+   }
+      return Integer.parseInt(den);
+   }
+
+   
+   
+   //Finds the result of the numerators
+   public static int findNumResult(int num1, int den1, int num2, int den2, String operator) {
+      int numResult = 0;
+      //If adding then cross multiply the numerators and denominators and add
+      if (operator.equals("+")) {
+         numResult = (num1 * den2) + (num2 * den1);
+         }
+      //If subtracting then cross multiply the numerators and denominators and subtract
+      else if (operator.equals("-")) {
+         numResult = (num1 * den2) - (num2 * den1);
+      }
+      //If multiplying then just multiply the numerators across from each other
+      else if (operator.equals("*")) {
+         numResult = num1 * num2;
+      }
+
+      //If dividing then multiply the first numerator and second denominator
+      else if (operator.equals("/")) {
+         numResult = num1 * den2;
+         
+      }
+      return numResult;
 
    }
+
+   //Finds the denominator results
+   public static int findDenResult(int num1, int den1, int num2, int den2,  String operator) {
+      int denResult = 0;
+      //If dividing then multiply the first denominator and second numerator
+      if (operator.equals("/")) {
+         denResult = den1 * num2;
+      }
+      //Else always just multiply both denominators
+      else {
+         denResult = den1 * den2;
+      } 
+      return denResult;
+   }
+   
+   //Finds the GCD
+   public static int findGCD(int numResult, int denResult) {
+      //Get absolute values of the numerators and the denominators
+      if (numResult < 0) {
+         numResult *= -1;
+      }
+      if (denResult < 0) {
+         denResult *= -1;
+      }
+
+      //While the denominator is not zero denResult would be the numResult mod denResult and numResult would be the previous denResult
+      while (denResult != 0) {
+         int x = denResult;
+         denResult = numResult % denResult;
+         numResult = x;
+      }
+      //Return numResult as GCD
+      return numResult;
+   }
+
+   //Simplifies and formats the end result
+   public static String Simplify(int numResult, int denResult, int wholeResult, String result) {
+         //If the denominator has a negative remove it and set the numerator negative
+         if (denResult < 0) {
+            numResult *= -1;
+            denResult *= -1;
+         }
+
+         //Get GCD and divide the numerator and denominator by it
+         int gcd = findGCD(numResult, denResult);
+         numResult /= gcd;
+         denResult /= gcd;
+         
+         int sign = 1;
+         if (numResult < 0) {
+            sign = -1;
+            numResult *= -1;
+         }
+
+         //If improper fraction change to mixed number
+         if (numResult >= denResult) {
+            wholeResult = numResult / denResult;
+            numResult = numResult % denResult;
+         }
+         
+         if(wholeResult == 0) {
+            numResult *= sign;
+         }
+         else {
+            wholeResult *= sign;
+         }
+         
+         //If there is no numerator the result would be the wholeResult
+         if (numResult == 0) {
+            result = Integer.toString(wholeResult);
+         }
+         //If there is no whole number then the result would just be the numerator over the denominator
+         else if(wholeResult == 0) {
+            result = numResult + "/" + denResult;
+         }
+
+         //Else the result would be the whole reuslt, numResult, and denResult
+         else {
+            result = wholeResult + " " + numResult + "/" + denResult; 
+         }
+
+
+         //Return the result to process expression
+         return result;
+      }
+      
+   
+ 
    
 
+ 
    // Returns a string that is helpful to the user about how
    // to use the program. These are instructions to the user.
    public static String provideHelp() {
-      // TODO: Update this help text!
-     
-      String help = "Welcome to the fraction calculator. Please enter a mathematical expression of your choosing. You can add(+), subtract, multiply(*), and divide(/) whole integers and fractions. Please make sure to space out each term.\n";
+      String help = "Welcome to the fraction calculator. Please enter a mathematical expression of your choosing. \nYou can add(+), subtract, multiply(*), and divide(/) whole integers and fractions, mixed included. \nPlease make sure to space out each term. \nIf you wish to operate on mixed fractions please seperate the whole number and the fraction with an underscore (_).\n";
       return help;
    }
 }
